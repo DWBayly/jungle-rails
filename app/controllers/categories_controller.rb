3223c61e0ca51d29721a68e008009a6a82616080
DWBayly <dbayly@uvic.ca>
2017-12-07 23:04:52 +0000

    Added admin/categories route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,25 @@
 class CategoriesController < ApplicationController
-
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+  def index
+    @category = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @product.save
+      redirect_to [:admin, :category], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
 
 end
